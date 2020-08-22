6b785f3b3edb725d3b807c0d3e03a437594a5a22
Xelanor <berkeozelsel@gmail.com>
2020-08-22 17:10:35 +0300

    Auth context - bottom navigation

--- src/navigation/HomeStack.js
+++ src/navigation/HomeStack.js
@@ -1,6 +1,43 @@
 import React from 'react';
+import {Image} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
 import HomeScreen from '../screens/HomeScreen';
+import BasketScreen from '../screens/BasketScreen';
+
+const Tab = createMaterialBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator
+      initialRouteName="Home"
+      activeColor="#EAC100"
+      inactiveColor="#f0edf6"
+      labeled={false}
+      barStyle={{backgroundColor: '#10316B'}}>
+      <Tab.Screen
+        options={{
+          tabBarIcon: ({color}) => (
+            <MaterialCommunityIcons name="home" color={color} size={26} />
+          ),
+        }}
+        name="Home"
+        component={HomeScreen}
+      />
+      <Tab.Screen
+        options={{
+          tabBarIcon: ({color}) => (
+            <MaterialCommunityIcons name="account" color={color} size={26} />
+          ),
+        }}
+        name="Profile"
+        component={BasketScreen}
+      />
+    </Tab.Navigator>
+  );
+}
 
 const SportistStack = createStackNavigator();
 
@@ -18,7 +55,24 @@ export default function SportistApp() {
           fontSize: 22,
         },
       }}>
-      <SportistStack.Screen name="Home" component={HomeScreen} />
+      <SportistStack.Screen
+        options={{
+          headerTitle: (props) => (
+            <Image
+              style={{
+                height: 40,
+                resizeMode: 'contain',
+              }}
+              source={{
+                uri:
+                  'https://res.cloudinary.com/drs642d4b/image/upload/v1598094982/Frame_1_xaqpvg.png',
+              }}
+            />
+          ),
+        }}
+        name="Homepage"
+        component={MyTabs}
+      />
     </SportistStack.Navigator>
   );
 }
--- src/navigation/Routes.js
+++ src/navigation/Routes.js
@@ -1,8 +1,31 @@
 import React, {useContext, useState, useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import auth from '@react-native-firebase/auth';
+
+import {AuthContext} from './AuthProvider';
 import HomeStack from './HomeStack';
+import Loading from '../components/Loading';
 
 export default function Routes() {
+  const {user, setUser} = useContext(AuthContext);
+  const [loading, setLoading] = useState(true);
+  const [initializing, setInitializing] = useState(true);
+
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  });
+
+  if (loading) {
+    return <Loading />;
+  }
+
   return (
     <NavigationContainer>
       <HomeStack />
